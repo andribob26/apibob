e2dc2ded1f5b04f26cc4cb9899d8d8915e6f2be8
andribob26 <74372832+andribob26@users.noreply.github.com>
2022-06-16 21:24:40 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 const config = {
     database: {
-        url: "mongodb+srv://andribob26:w2qRfip1Ts7JNQ5P@clusterbob.tv73mzt.mongodb.net/?retryWrites=true&w=majority"
+        url: "mongodb+srv://namakamu:passwordkamu@clusterbob.tv73mzt.mongodb.net/?retryWrites=true&w=majority"
     },
     clodinary:{
-        cloudName:"andribob",
-        apiKey:"871573944547912",
-        apiSecret:"Grq7dlCv5lo0TzmJGxh9bLM2Mpk"
+        cloudName:"namakamu",
+        apiKey:"apikamu",
+        apiSecret:"apisecretkamu"
     },
     secret: "!@#MYSECRETEP@SSWORD!@#"
 }
